5b6e598b2d4f919e6225b3240a69adbb5e903721
Your Name <you@example.com>
2024-05-16 08:43:30 -0700

    Put all data into products.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,29 +1,3 @@
-const products = [{
-  image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-  name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-  rating: {
-    starts: 4.5,
-    count: 87
-  },
-  price: 1090
-}, {
-  image: 'images/products/intermediate-composite-basketball.jpg',
-  name: 'Intermediate Size Basketball',
-  rating: {
-    starts: 4,
-    count: 127
-  },
-  price: 2095
-}, {
-  image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-  name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-  rating: {
-    starts: 4.5,
-    count: 56
-  },
-  price: 799
-}];
-
 // Store all product elements as a string
 let productsHTML = '';
 
@@ -41,7 +15,7 @@ products.forEach((product) => {
 
       <div class="product-rating-container">
         <img class="product-rating-stars"
-          src="images/ratings/rating-${product.rating.starts * 10}.png">
+          src="images/ratings/rating-${product.rating.stars * 10}.png">
         <div class="product-rating-count link-primary">
           ${product.rating.count}
         </div>
